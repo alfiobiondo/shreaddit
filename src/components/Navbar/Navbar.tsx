e04a33958151de841f6c4e5b5f97d591ca99a8be
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2024-09-03 22:21:15 +0200

    change reddit logo text add SearchInput

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
+import SearchInput from './SearchInput';
 
 const Navbar: React.FC = () => {
 	return (
@@ -11,12 +12,15 @@ const Navbar: React.FC = () => {
 					height='30px'
 				/>
 				<Image
-					src='/images/redditText.svg'
+					src='/images/shredditText.svg'
 					alt='reddit logo text'
 					height='46px'
 					display={{ base: 'none', md: 'unset' }}
 				/>
 			</Flex>
+			<SearchInput />
+			{/* 			<Directory/>
+			<RightContent/> */}
 		</Flex>
 	);
 };
