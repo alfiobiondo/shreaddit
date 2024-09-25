85b747cf9cfeca25b96469fbff27f509764a2013
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2024-09-25 16:42:46 +0200

    add handleCreatePost function to save posts to firestore and upload image to firebase storage

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -1,4 +1,12 @@
-import { Flex, Icon } from '@chakra-ui/react';
+import {
+	Alert,
+	AlertDescription,
+	AlertIcon,
+	AlertTitle,
+	Flex,
+	Icon,
+	Text,
+} from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { BsLink45Deg, BsMic } from 'react-icons/bs';
 import { IoDocumentText, IoImageOutline } from 'react-icons/io5';
@@ -6,8 +14,22 @@ import { BiPoll } from 'react-icons/bi';
 import TabItem from './TabItem';
 import TextInputs from './PostForm/TextInputs';
 import ImageUpload from './PostForm/ImageUpload';
+import { Post } from '@/atoms/postsAtom';
+import { User } from 'firebase/auth';
+import { useRouter } from 'next/router';
+import {
+	addDoc,
+	collection,
+	serverTimestamp,
+	Timestamp,
+	updateDoc,
+} from 'firebase/firestore';
+import { firestore, storage } from '@/firebase/clientApp';
+import { getDownloadURL, ref, uploadString } from 'firebase/storage';
 
-type NewPostFormProps = {};
+type NewPostFormProps = {
+	user: User;
+};
 
 const formTabs: TabItem[] = [
 	{
@@ -37,7 +59,8 @@ export type TabItem = {
 	icon: typeof Icon.arguments;
 };
 
-const NewPostForm: React.FC<NewPostFormProps> = () => {
+const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
+	const router = useRouter();
 	const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
 	const [textInputs, setTextInput] = useState({
 		title: '',
@@ -45,8 +68,46 @@ const NewPostForm: React.FC<NewPostFormProps> = () => {
 	});
 	const [selectedFile, setSelectedFile] = useState<string>();
 	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(false);
 
-	const handleCreatePost = async () => {};
+	const handleCreatePost = async () => {
+		const { communityId } = router.query;
+		// create new post object => type Post
+		const newPost: Post = {
+			communityId: communityId as string,
+			creatorId: user.uid,
+			creatorDisplayName: user.email!.split('@')[0],
+			title: textInputs.title,
+			body: textInputs.body,
+			numberOfComments: 0,
+			voteStatus: 0,
+			createdAt: serverTimestamp() as Timestamp,
+		};
+
+		setLoading(true);
+		try {
+			// store the post in db
+			const postDocRef = await addDoc(collection(firestore, 'posts'), newPost);
+
+			// check for selectedFile
+			if (selectedFile) {
+				// store in storeg => getDownloadURL (return imageURL)
+				const imageRef = ref(storage, `posts/${postDocRef.id}/image`);
+				await uploadString(imageRef, selectedFile, 'data_url');
+				const downloadURL = await getDownloadURL(imageRef);
+
+				// update post doc by adding imageURL
+				await updateDoc(postDocRef, { imageURL: downloadURL });
+			}
+		} catch (error: any) {
+			console.log('handleCreatePost error', error.message);
+			setError(true);
+		}
+		setLoading(false);
+
+		// redirect the user back to the communityPage using the router
+		// router.back();
+	};
 
 	const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const reader = new FileReader();
@@ -104,6 +165,12 @@ const NewPostForm: React.FC<NewPostFormProps> = () => {
 					/>
 				)}
 			</Flex>
+			{error && (
+				<Alert status='error'>
+					<AlertIcon />
+					<Text mr={2}>Error creating post</Text>
+				</Alert>
+			)}
 		</Flex>
 	);
 };
