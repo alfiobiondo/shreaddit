5d52f85a2452392b534f251f4752d73ff6b73537
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2024-10-04 09:06:52 +0200

    finish create comment functionality

--- src/components/Posts/Comments/Comments.tsx
+++ src/components/Posts/Comments/Comments.tsx
@@ -1,4 +1,4 @@
-import { Post } from '@/atoms/postsAtom';
+import { Post, postState } from '@/atoms/postsAtom';
 import { Box, Flex } from '@chakra-ui/react';
 import { User } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
@@ -7,10 +7,12 @@ import {
 	Timestamp,
 	collection,
 	doc,
+	increment,
 	serverTimestamp,
 	writeBatch,
 } from 'firebase/firestore';
 import { firestore } from '@/firebase/clientApp';
+import { useSetRecoilState } from 'recoil';
 
 type CommentsProps = {
 	user: User;
@@ -35,12 +37,13 @@ const Comments: React.FC<CommentsProps> = ({
 	communityId,
 }) => {
 	const [commentText, setCommentText] = useState('');
-	const [comments, setComments] = useState([]);
+	const [comments, setComments] = useState<Comment[]>([]);
 	const [fetchLoading, setFetchLoading] = useState(false);
 	const [createLoading, setCreateLoading] = useState(false);
+	const setPostState = useSetRecoilState(postState);
 
 	const onCreateComment = async () => {
-		// update post numberOfComments +1
+		setCreateLoading(true);
 		try {
 			const batch = writeBatch(firestore);
 
@@ -59,11 +62,30 @@ const Comments: React.FC<CommentsProps> = ({
 			};
 
 			batch.set(commentDocRef, newComment);
+
+			// update post numberOfComments +1
+			const postDocRef = doc(firestore, 'posts', selectedPost?.id!);
+
+			batch.update(postDocRef, {
+				numberOfComments: increment(1),
+			});
+
+			batch.commit();
+
+			// update client recoil state
+			setCommentText('');
+			setComments((prev) => [newComment, ...prev]);
+			setPostState((prev) => ({
+				...prev,
+				selectedPost: {
+					...prev.selectedPost,
+					numberOfComments: prev.selectedPost?.numberOfComments! + 1,
+				} as Post,
+			}));
 		} catch (error) {
 			console.log('onCreateComment error', error);
 		}
-
-		// update client recoil state
+		setCreateLoading(false);
 	};
 
 	const onDeleteComment = async (comment: any) => {
