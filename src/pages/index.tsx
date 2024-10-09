28adb60a6bb77aaaee56bb3628d195c53cc21c9e
Alfio Biondo <122615323+alfiobiondo@users.noreply.github.com>
2024-10-09 21:46:38 +0200

    add Top Communities component and supporting components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { CommunityState } from '@/atoms/communityAtom';
 import { Post, PostVote } from '@/atoms/postsAtom';
 import CreatePostLink from '@/components/Community/CreatePostLink';
+import PersonalHome from '@/components/Community/PersonalHome';
+import Premium from '@/components/Community/Premium';
+import Recommendations from '@/components/Community/Recommendations';
 import PageContent from '@/components/Layout/PageContent';
 import PostItem from '@/components/Posts/PostItem';
 import PostLoader from '@/components/Posts/PostLoader';
@@ -166,7 +169,11 @@ const Home: NextPage = () => {
 					</Stack>
 				)}
 			</>
-			<>{/* Recommendations */}</>
+			<Stack spacing={5}>
+				<Recommendations />
+				<Premium />
+				<PersonalHome />
+			</Stack>
 		</PageContent>
 	);
 };
